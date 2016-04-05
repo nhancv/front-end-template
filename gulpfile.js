e2ef7260247afce02f5e78ee21aeb3460edae076
Nhan Cao <nhancv1992@gmail.com>
2016-04-05 23:00:09 +0530

    minify images task: gulp imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,23 @@ var gulp = require('gulp');
 
 // include plug-ins
 var jshint = require('gulp-jshint');
+var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
 
 // JS hint task
 gulp.task('jshint', function() {
     gulp.src('./src/scripts/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
+});
+
+// minify new images
+gulp.task('imagemin', function() {
+    var imgSrc = './src/images/**/*',
+        imgDst = './build/images';
+
+    gulp.src(imgSrc)
+        .pipe(changed(imgDst))
+        .pipe(imagemin())
+        .pipe(gulp.dest(imgDst));
 });
